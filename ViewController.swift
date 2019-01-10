77d727f07973fb07cf23b1b85bda445f5c38419e
AsafDavidov <adavidov@conncoll.edu>
2019-01-10 18:36:34 -0500

    Complete

--- ViewController.swift
+++ ViewController.swift
@@ -9,16 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textDisplay: UILabel!
+    
+    @IBAction func helloWorld(_ sender: Any) {
+        print("Pressed Hello World button")
+        textDisplay.text = "Hello, world!"
+    }
+    @IBAction func unicorn(_ sender: Any) {
+        print("Pressed unicorn")
+        textDisplay.text = "🦊"
+    }
+    @IBAction func goodbyeWorld(_ sender: Any) {
+        print("Pressed Goodbye World button")
+        textDisplay.text = "goodbye, world!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz..."
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    
-
-   
-
 }
